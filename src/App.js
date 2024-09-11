265a12ee5b341b2489cd5a5567a73c7ff1f41fb4
Octavio García Larrecharte <okigarcia06@gmail.com>
2024-09-10 21:05:31 -0300

    Agregando steps y nombres

--- src/App.js
+++ src/App.js
@@ -1,68 +1,70 @@
-import React, { useState, useMemo, useCallback } from "react";
-import { NextUIProvider, Button, ButtonGroup } from "@nextui-org/react";
+  // paso 1: Seleccionar tipo de carga (con algún elemento tipo droplist que liste los tipos de carga y seleccione 1). 
+  // Paso 2: Ingresar datos de retiro (acá basicamente un form con campos tipo textbox para calle, nro - droplist para provincia y localidad) *Acá deberíamos preguntar que provincias quiere que carguemos y que localidades* y un text área para la referencia. (Tenemos que validar todos los campos obligatorios, y por ej en nro que se ingrese un número, referencia es opcional) Por último un elemento tipo calendar para elegir la fecha de retiro. (validar que sea mayor o igual a hoy básicamente las fechas anteriores a la actual no se pueden seleccionar).
+  // Paso 3: Ingresar datos de entrega (Básicamente lo mismo que el paso 2, validando que la fecha de retiro sea mayor o igual a la fecha seleccionada en el retiro y validar que el domicilio de retiro no sea igual al de entrega).
+  // Paso 4: Publicar el pedido (en esta parte básicamente hacemos un inputloader para subir fotos (validar que sea solo en el formato especificado jpg o png, max 3 fotos, no mas de 10mb) y por último un botón de publicar que cuando lo aprieten se envíe la notificación por email y la notificación push. 
 
-import Stepper from "./components/Stepper";
-
-import Step1 from "./components/Step1";
-import Step2 from "./components/Step2";
-import Step3 from "./components/Step3";
-import Step4 from "./components/Step4";
-import Step5 from "./components/Step5";
-
-const App = () => {
-  const [currentStep, setCurrentStep] = useState(1);
-  const isFirstStep = currentStep === 1;
-  const isLastStep = currentStep === 5;
-
-  const CurrentForm = useMemo(() => {
-    switch (currentStep) {
-      case 1:
-        return <Step1 />;
-      case 2:
-        return <Step2 />;
-      case 3:
-        return <Step3 />;
-      case 4:
-        return <Step4 />;
-      case 5:
-        return <Step5 />;
-
-      default:
-        break;
-    }
-  }, [currentStep]);
-
-  const handleBack = useCallback(() => {
-    setCurrentStep((currentStep) => {
-      return currentStep - 1;
-    });
-  }, []);
-
-  const handleForward = useCallback(() => {
-    setCurrentStep((currentStep) => {
-      return currentStep + 1;
-    });
-  }, []);
-
-  return (
-    <NextUIProvider>
-      <div class="container mx-auto p-10">
-        <Stepper currentStep={currentStep} />
-        {CurrentForm}
-        <ButtonGroup>
-          <Button color="default" onPress={handleBack} isDisabled={isFirstStep}>
-            {"Atrás"}
-          </Button>
-          <Button
-            color={isLastStep ? "danger" : "primary"}
-            onPress={isLastStep ? undefined : handleForward}
-          >
-            {isLastStep ? "Pagar" : "Siguiente"}
-          </Button>
-        </ButtonGroup>
-      </div>
-    </NextUIProvider>
-  );
-};
-
-export default App;
+  import React, { useState, useMemo, useCallback } from "react";
+  import { NextUIProvider, Button, ButtonGroup } from "@nextui-org/react";
+  
+  import Stepper from "./components/Stepper";
+  
+  import Step1 from "./components/Step1";
+  import Step2 from "./components/Step2";
+  import Step3 from "./components/Step3";
+  import Step4 from "./components/Step4";
+  
+  const App = () => {
+    const [currentStep, setCurrentStep] = useState(1);
+    const isFirstStep = currentStep === 1;
+    const isLastStep = currentStep === 4;
+  
+    const CurrentForm = useMemo(() => {
+      switch (currentStep) {
+        case 1:
+          return <Step1 />;
+        case 2:
+          return <Step2 />;
+        case 3:
+          return <Step3 />;
+        case 4:
+          return <Step4 />;
+        default:
+          break;
+      }
+    }, [currentStep]);
+  
+    const handleBack = useCallback(() => {
+      setCurrentStep((currentStep) => {
+        return currentStep - 1;
+      });
+    }, []);
+  
+    const handleForward = useCallback(() => {
+      setCurrentStep((currentStep) => {
+        return currentStep + 1;
+      });
+    }, []);
+  
+    return (
+      <NextUIProvider>
+        <div className="container mx-auto p-10">
+          <Stepper currentStep={currentStep} />
+          {CurrentForm}
+          <ButtonGroup>
+            <Button color="default" onPress={handleBack} isDisabled={isFirstStep}>
+              {"Atrás"}
+            </Button>
+            <Button
+              color={isLastStep ? "danger" : "primary"}
+              onPress={isLastStep ? undefined : handleForward}
+            >
+              {isLastStep ? "Publicar" : "Siguiente"}
+            </Button>
+          </ButtonGroup>
+        </div>
+      </NextUIProvider>
+    );
+  };
+  
+  export default App;
+  
